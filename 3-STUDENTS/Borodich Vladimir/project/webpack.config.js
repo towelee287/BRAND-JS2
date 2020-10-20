dc2d29817dac505ed274befcc3aa6791160720e8
wowankz <wowikwow@mail.ru>
2020-10-20 22:17:28 +0500

    Поработал с настройками webpack.config.js. Сделал классами Basket.js and catalog.js

--- 3-STUDENTS/Borodich Vladimir/project/webpack.config.js
+++ 3-STUDENTS/Borodich Vladimir/project/webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
                     {
                         loader: MiniCssExtractPlugin.loader,
                         options: {
-                            publicPath: '../',
+                            publicPath: './../',
                         },
                     },
                     'css-loader'
