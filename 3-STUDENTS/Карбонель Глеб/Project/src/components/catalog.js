bae43e105351adf2c7e3246beeb9970cd70e5de2
Gleb <karbonelg@mail.ru>
2020-10-25 01:26:58 +0300

    Написан класс Catalog

--- 3-STUDENTS/Карбонель Глеб/Project/src/components/catalog.js
+++ 3-STUDENTS/Карбонель Глеб/Project/src/components/catalog.js
@@ -1,12 +1,16 @@
-export default {
-    items: [],
-    basket: null,
-    container: null,
-    imgFTPurl: 'https://raw.githubusercontent.com/kellolo/static/master/img/JS1_shop',
-    url: 'https://raw.githubusercontent.com/kellolo/static/master/JSON/catalog.json',
-    init(basket) {
-        this.container = document.querySelector('#catalog');
+import CatalogItem from './catalogItem.js';
+
+export default class Catalog {
+
+    constructor(basket, container = '#catalog', url = '/catalog.json'){
+        this.items = [];
         this.basket = basket;
+        this.container = document.querySelector(container);
+        this.url = 'https://raw.githubusercontent.com/kellolo/static/master/JSON' + url;
+        this._init();
+    }
+
+    _init() {
         this._get(this.url)
         .then(items => {
             this.items = items;
@@ -17,51 +21,31 @@ export default {
         })
 
         // this._handleEvents();
-    },
+    }
+
     _get(url) {
         return fetch(url).then(d => d.json()) //на выходе из этого метода вы получите полноценный объект(массив) с данными
-    },
+    }
+
     _handleEvents() {
         this.container.addEventListener('click', e => {
             if (e.target.name == 'add') {
-                let item = {
+                this.basket.add({
                     productId: e.target.dataset.id,
                     productImg: e.target.dataset.img,
                     productName: e.target.dataset.name,
-                    productPrice: +e.target.dataset.price,
-                };
-                this.basket.add(item)
+                    productPrice: +e.target.dataset.price
+                })
             }
         })
-    },
+    }
+
     _render() {
         let htmlStr = '';
-        this.items.forEach((item, index) => {
-            let imgURL = `${this.imgFTPurl}/featuredItem${index + 1}.jpg`;
-            htmlStr += `
-            <div class="hot-offer">
-                <div class="hot-offer__shadow">
-                    <img src="${imgURL}" alt="t-shirt">
-                    <div class="hot-offer__hover">
-                        <div class="hot-offer__square">
-                            <button 
-                                name="add"
-                                data-id="${item.productId}"
-                                data-name="${item.productName}"
-                                data-img="${imgURL}"
-                                data-price="${item.productPrice}"
-                            ><img src="../src/assets/img/kart_white.png" alt="order">Add to Cart</button>
-                        </div>
-                    </div>
-                </div>
-                <a href="single.html">${item.productName}</a>
-                <h3>
-                    $${item.productPrice}
-                    <!--stars-->
-                </h3>
-            </div>
-            `;
+        this.items.forEach((item) => {
+            htmlStr += new CatalogItem(item).render();
         });
         this.container.innerHTML = htmlStr;
     }
+
 }
