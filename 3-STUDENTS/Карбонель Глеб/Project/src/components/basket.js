155d8bf3c70e32d1c8821f5c1def612e28147488
Gleb <karbonelg@mail.ru>
2020-10-25 01:24:07 +0300

    Написан класс Basket

--- 3-STUDENTS/Карбонель Глеб/Project/src/components/basket.js
+++ 3-STUDENTS/Карбонель Глеб/Project/src/components/basket.js
@@ -1,13 +1,17 @@
-class Basket {
-    constructor(wrapper, container, url){
+import BasketItem from './basketItem.js';
+
+export default class Basket {
+
+    constructor(container = '#basket', url = '/basket.json'){
         this.items = [];
-        this.wrapper = wrapper;
-        this.container = container;
-        this.url = url;
+        this.wrapper = null;
+        this.container = document.querySelector(container);
+        this.url = 'https://raw.githubusercontent.com/kellolo/static/master/JSON/' + url;
+        this._init();
     }
-    init() {
+
+    _init() {
         this.wrapper = document.querySelector('.drop-cart');
-        this.container = document.querySelector('#basket');
 
         this._get(this.url)
             .then(basketObject => {
@@ -22,6 +26,7 @@ class Basket {
     _get(url) {
         return fetch(url).then(d => d.json()) //на выходе из этого метода вы получите полноценный объект(массив) с данными
     }
+
     _handleEvents() {
         document.querySelector('#toggle-basket').addEventListener('click', () => {
             this.wrapper.classList.toggle('hidden');
@@ -34,6 +39,7 @@ class Basket {
             }
         })
     }
+
     add(item) {
         // console.log('add ' + item.productName)
         let find = this.items.find(basketItem => basketItem.productId == item.productId);
@@ -58,32 +64,8 @@ class Basket {
     _render() {
         let htmlStr = '';
         this.items.forEach((item) => {
-            htmlStr += `
-            <div class="drop-cart__product">
-                <a href="product.html" class="drop-cart__product-link">
-                    <img src="${item.productImg}"
-                        alt="product" class="drop-cart__product-img">
-                </a>
-                <div class="drop-cart__product-info">
-                    <a href="product.html" class="drop-cart__product-name">${item.productName}</a>
-                    <div class="drop-cart__product-stars">
-                        <i class="${item.stars > "0" ? "fas" : "far"} ${item.stars === "0.5" ? "fa-star-half-alt" : "fa-star"}"></i>
-                        <i class="${item.stars > "1" ? "fas" : "far"} ${item.stars === "1.5" ? "fa-star-half-alt" : "fa-star"}"></i>
-                        <i class="${item.stars > "2" ? "fas" : "far"} ${item.stars === "2.5" ? "fa-star-half-alt" : "fa-star"}"></i>
-                        <i class="${item.stars > "3" ? "fas" : "far"} ${item.stars === "3.5" ? "fa-star-half-alt" : "fa-star"}"></i>
-                        <i class="${item.stars > "4" ? "fas" : "far"} ${item.stars === "4.5" ? "fa-star-half-alt" : "fa-star"}"></i>
-                    </div>
-                    <div class="drop-cart__product-price">
-                        <span class="drop-cart__product-count">${item.amount} </span> x ${item.productPrice}
-                        <span class="drop-cart__product-sum"> = $${item.productPrice * item.amount}</span>
-                    </div>
-                </div>
-                <a href="#" data-id="${item.productId}" name="remove" class="drop-cart__product-close far fa-times-circle"></a>
-            </div>
-        `;
+            htmlStr += new BasketItem(item).render();
         });
         this.container.innerHTML = htmlStr;
     }
 }
-
-export default new Basket(null, null, 'https://raw.githubusercontent.com/kellolo/static/master/JSON/basket.json');
