b08cdcceac77fbbad2c375c670079cbc989865a9
voidf.0xb3 <the.invisible.phantom@gmail.com>
2020-10-27 22:14:58 +0300

    Remove getItems from Catalog.js

--- 3-STUDENTS/AndreevValentin/Project/src/components/Catalog.js
+++ 3-STUDENTS/AndreevValentin/Project/src/components/Catalog.js
@@ -1,27 +1,5 @@
 import CatalogItem from "./CatalogItem.js";
 
-function getItems() {
-	// This should be retrieved from a server...
-	return {
-		0: new CatalogItem(0, "Mango people T-shirt", 5200,
-			"https://raw.githubusercontent.com/VoidPhantom/gbimg/master/0.jpg"),
-		1: new CatalogItem(1, "Banana people T-shirt", 5300,
-			"https://raw.githubusercontent.com/VoidPhantom/gbimg/master/1.jpg"),
-		2: new CatalogItem(2, "Strawberry people T-shirt", 5500,
-			"https://raw.githubusercontent.com/VoidPhantom/gbimg/master/2.jpg"),
-		3: new CatalogItem(3, "Orange people T-shirt", 6700,
-			"https://raw.githubusercontent.com/VoidPhantom/gbimg/master/3.jpg"),
-		4: new CatalogItem(4, "Pumpkin people T-shirt", 6900,
-			"https://raw.githubusercontent.com/VoidPhantom/gbimg/master/4.jpg"),
-		5: new CatalogItem(5, "Pineapple people T-shirt", 9400,
-			"https://raw.githubusercontent.com/VoidPhantom/gbimg/master/5.jpg"),
-		6: new CatalogItem(6, "Cucumber people T-shirt", 2300,
-			"https://raw.githubusercontent.com/VoidPhantom/gbimg/master/6.jpg"),
-		7: new CatalogItem(7, "Tomato people T-shirt", 4500,
-			"https://raw.githubusercontent.com/VoidPhantom/gbimg/master/7.jpg")
-	};
-}
-
 export default class Catalog {
 	constructor(cart, container, itemsPerRow) {
 		this.cart = cart;
