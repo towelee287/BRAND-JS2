a0e57a3654bff01ecee4483949b97f15793e7cf9
kellolo <87kell@gmail.com>
2020-11-01 13:22:51 +0200

    6

---
+++ 2-Project/6/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <ComponentOne />
+        <component-one />
+    </div>
+</template>
+
+<script>
+// import ComponentOne
+
+export default {
+    // data: {} для компонентов это не верно
+    data() {
+        return {
+            
+        }
+    },
+    components: { ComponentOne }, //для дочерних компонентов
+    // props: []
+    props: {  } //свойства, передаваемые извне (внутри компонента их менять нельзя)
+
+}
+</script>
+
+<style>
+
+</style>
